30db146e563df9d1e712053f292c29d83f6fd88b
Kya <1590981348@qq.com>
2025-08-18 23:30:04 +0800

    feat: 34 思路修正

--- 2.二分算法/34.在排序数组中查找元素第一个和最后一个位置.js
+++ 2.二分算法/34.在排序数组中查找元素第一个和最后一个位置.js
@@ -3,7 +3,18 @@
  * @param {number} target
  * @return {number[]}
  */
+
+// 二分查找解法：先封装一个二分查找的方法
+let binarySearch = (nums,target) => {
+  
+}
+
 var searchRange = function(nums, target) {
-  const res = [-1, -1]; // 未找到不更新res，直接返回[-1,-1]
+  let res = [-1, -1]; // 未找到不更新res，直接返回[-1,-1]
   return res;
 };
+
+
+
+// 输入：nums = [5,7,7,8,8,10,12], target = 8
+// 输出：[3,4]
