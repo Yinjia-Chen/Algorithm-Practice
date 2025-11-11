2334bc307dbff66069e5406e27b534f1250a2efe
Kya <1590981348@qq.com>
2025-11-11 22:49:58 +0800

    feat: 876链表中点左右侧双写法及原理

--- Hot 100/链表/876.链表的中间结点.js
+++ Hot 100/链表/876.链表的中间结点.js
@@ -18,3 +18,16 @@ var middleNode = function(head) {
   }
   return slow;
 };
+
+// 上述算法对于偶数个结点的链表，返回中间右侧的结点
+// 下述算法对于偶数个结点的链表，返回中间左侧的结点
+var middleNode = function(head) {
+  let slow = head;
+  let fast = head;
+  // 通过提前结束循环来让中点停在左侧
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
