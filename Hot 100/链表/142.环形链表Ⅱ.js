016d1e36c8d88bf2a6f9833c3a4fed75dac4f299
陈尹嘉 <chenyj@dunhefund.com>
2025-11-14 12:24:30 +0800

    hot100: 142环形链表Ⅱ (未完成)

--- Hot 100/链表/142.环形链表Ⅱ.js
+++ Hot 100/链表/142.环形链表Ⅱ.js
@@ -10,6 +10,17 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var detectCycle = function(head) {
-  
+// & 1.哈希表  时间 O(N) 空间 O(N)
+// 这里 map 校验时，必须要用 map.has：当索引为 0 时，if(0) 判断为假，将无法正确返回
+var detectCycle = function (head) {
+  const map = new Map();
+  let curr = head;
+  let pos = -1;
+  while (curr) {
+    pos++;
+    if (map.has(curr)) return curr;
+    map.set(curr, pos)
+    curr = curr.next;
+  }
+  return null;
 };
