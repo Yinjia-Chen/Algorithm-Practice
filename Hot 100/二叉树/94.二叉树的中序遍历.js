83cbb2247c436a7ead29f0a0b8d54fa48afdd05f
Kya <1590981348@qq.com>
2025-11-21 00:09:49 +0800

    hot100: 94init

---
+++ Hot 100/二叉树/94.二叉树的中序遍历.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// 递归
+var inorderTraversal = function(root) {
+  const mid = [];
+  // 终止条件：当前节点为 null
+  if (!root) return;
+  
+};
+// 迭代
+// Morris
