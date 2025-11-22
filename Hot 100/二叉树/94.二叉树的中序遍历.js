0d83df288f390993fca068213a6a11be031918a2
Kya <1590981348@qq.com>
2025-11-22 10:36:38 +0800

    hot100: 94二叉树中序遍历(未完成)

--- Hot 100/二叉树/94.二叉树的中序遍历.js
+++ Hot 100/二叉树/94.二叉树的中序遍历.js
@@ -10,12 +10,27 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-// 递归
-var inorderTraversal = function(root) {
+// & 1. 递归 
+var inorderTraversal = function (root) {
+  // 结果数组
   const mid = [];
-  // 终止条件：当前节点为 null
-  if (!root) return;
-  
+  // 递归开始：实际上是深度优先遍历算法 DFS
+  // 终止条件：当前节点为空，直接返回
+  // 根节点 与 左右子树的关系：整个二叉树都是由 “左子树 + 根节点 + 右子树” 组成
+  // 中序遍历：左子树 -> 根 -> 右子树
+  // 因此递归先进入左子树，接着到 根节点，再进入 右子树
+  const dfs = (node) => {
+    // 终止条件
+    if (!node) return;
+    // 中序遍历：左 根 右
+    dfs(node.left);
+    mid.push(node.val); // 归：将节点值推入数组
+    dfs(node.right);
+  };
+  // 对 root 为根节点的树开展中序遍历
+  dfs(root);
+  return mid;
 };
+
 // 迭代
 // Morris
