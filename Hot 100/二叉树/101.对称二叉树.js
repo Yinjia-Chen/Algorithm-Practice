8dd316c5efba6a29d3d22a0f003f7ab09ffde54c
Kya <1590981348@qq.com>
2025-11-23 23:41:37 +0800

    hot100: 101对称二叉树(未完成)

---
+++ Hot 100/二叉树/101.对称二叉树.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+  if (!root) return true;
+  let ans = true;
+  const left = isSymmetric(root.left);
+  const right = isSymmetric(root.right);
+  ans = left === right ? true : false;
+  return ans;
+};
