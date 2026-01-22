09f2f736094dd07daf6e0ad65aa65a2cfba3d730
Kya <1590981348@qq.com>
2026-01-23 02:23:16 +0800

    feat: 字节专项复习

---
+++ 2026-2-2字节专项复习/一、数组 & 双指针/3.无重复字符的最长子串.ts
@@ -0,0 +1,22 @@
+/**
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ */
+function lengthOfLongestSubstring(s: string): number {
+  let res = 0;
+  let left = 0;
+  const set = new Set();
+  for (let right = 0; right < s.length; right++) {
+    const char = s[right];
+    while (set.has(char)) { // 进右端点，做校验-子串中不存在相同字符
+      set.delete(s[left]); // 删除子串最左端字符
+      left++; // 移动左端点，调整窗口
+    }
+    set.add(char); // 右端点进入窗口
+    res = Math.max(res, right - left + 1);
+  }
+  return res;
+}
+
+export default lengthOfLongestSubstring;
