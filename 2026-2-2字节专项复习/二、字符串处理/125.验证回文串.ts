539d851a31efa1b82d82f254dcadbcc638a76629
Kya <1590981348@qq.com>
2026-01-29 00:23:38 +0800

    feat: 字节专项复习

---
+++ 2026-2-2字节专项复习/二、字符串处理/125.验证回文串.ts
@@ -0,0 +1,19 @@
+/**
+ * 如果在将所有大写字符转换为小写字符、并移除所有非字母数字字符之后，短语正着读和反着读都一样。 则可以认为该短语是一个 回文串 。
+  字母和数字都属于字母数字字符。
+  给你一个字符串 s，如果它是 回文串 ，返回 true ；否则，返回 false 。
+ * 输入：s = "race a car"
+ * 输出：false
+ */
+function isPalindrome(s: string): boolean {
+  let cArr: string[] = [];
+  for (const c of s) {
+    if (c >= 'a' && c <= 'z' || c >= '0' && c <= '9') {
+      cArr.push(c);
+    }
+    else if (c >= 'A' && c <= 'Z') {
+      cArr.push(c.toLowerCase());
+    }
+  }
+  return cArr.join('') == cArr.slice().reverse().join('');
+};
